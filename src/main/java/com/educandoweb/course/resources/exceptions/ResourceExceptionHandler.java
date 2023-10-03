6e9dd2783de58b4c79b684dac86ed59970edf37b
Zeus Policarpo <zeusbr26@hotmail.com>
2023-10-03 18:18:17 -0300

    exception delete

--- src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -28,4 +28,17 @@ public class ResourceExceptionHandler {
 		
 	}
 	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandartError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		
+		StandartError err = new StandartError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+		
+	}
+	
+	
+	
 }
